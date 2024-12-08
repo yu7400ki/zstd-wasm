cb027144ce3cac909b01c0da3fdea5ffcd13efed
yu7400ki <yu7400ki@gmail.com>
2024-12-08 22:40:06 +0900

    format

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,4 @@
-import { type MainModule, type default as MainModuleFactory } from "./zstd";
+import type { MainModule, default as MainModuleFactory } from "./zstd";
 
 const mallocUint8Array = (wasm: MainModule, input: Uint8Array) => {
   const pointer = wasm._malloc(Uint8Array.BYTES_PER_ELEMENT * input.length);
@@ -14,19 +14,26 @@ const mallocUint8Array = (wasm: MainModule, input: Uint8Array) => {
   };
 };
 
+export const compressFactory =
+  (module: ReturnType<typeof MainModuleFactory>) =>
+  async (input: Uint8Array, level = 3) =>
+    module.then((wasm) => {
+      using pointer = mallocUint8Array(wasm, input);
+      const compressed: Uint8Array = wasm.compress(
+        pointer.value,
+        input.length,
+        level,
+      );
+      return compressed;
+    });
 
-export const compressFactory = (module: ReturnType<typeof MainModuleFactory>) => async (input: Uint8Array, level: number = 3) =>
-  module.then((wasm) => {
-    using pointer = mallocUint8Array(wasm, input);
-    const compressed: Uint8Array = wasm.compress(pointer.value, input.length, level);
-    return compressed;
-  })
-
-
-export const decompressFactory = (module: ReturnType<typeof MainModuleFactory>) => async (input: Uint8Array) =>
-  module.then((wasm) => {
-    using pointer = mallocUint8Array(wasm, input);
-    const decompressed: Uint8Array = wasm.decompress(pointer.value, input.length);
-    return decompressed;
-  })
-
+export const decompressFactory =
+  (module: ReturnType<typeof MainModuleFactory>) => async (input: Uint8Array) =>
+    module.then((wasm) => {
+      using pointer = mallocUint8Array(wasm, input);
+      const decompressed: Uint8Array = wasm.decompress(
+        pointer.value,
+        input.length,
+      );
+      return decompressed;
+    });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "bun:test";
+import { expect, test } from "bun:test";
 import { compress, decompress } from "../src";
 
 test("compress", async () => {
